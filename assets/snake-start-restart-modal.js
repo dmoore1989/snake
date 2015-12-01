f0cde83e83f1efddb53baf384e978b03e8b1e696
dmoore1989 <douglas.moore89@gmail.com>
2015-12-01 00:22:57 -0500

    Add a little more styling

--- assets/snake-start-restart-modal.js
+++ assets/snake-start-restart-modal.js
@@ -3,7 +3,7 @@
 
   StartModal = SG.StartModal = function ($el) {
     this.$el = $el;
-    this.highScore = 0
+    this.highScore = 0;
     this.renderStartModal();
     this.bindEvents();
   };
@@ -18,10 +18,12 @@
 
   StartModal.prototype.renderStartModal = function () {
     this.$el.empty();
-    this.$el.append("<h2>SNAKES ON A GAME</h2>");
-    this.$el.append("Controls: Use the Arrows to move the snake.  Eat as many apples you can without hitting a wall or yourself");
-    this.$el.append('<button class="start-button">PLAY!!!</button>');
+    this.$el.append('<h2 class="faded">Controls: Use the Arrows to move the snake.  Eat as many apples you can without hitting a wall or yourself</h3>');
+    this.$el.append('<button class="start-button faded">PLAY!!!</button>');
     this.$el.addClass('message');
+    setTimeout(function () {
+      $(".faded").removeClass("faded");
+    },100);
   };
 
   StartModal.prototype.renderRestartModal = function (score) {
@@ -35,6 +37,9 @@
     this.$el.append('<button class="start-button">Play Again?</button>');
     this.$el.addClass('message');
     this.bindEvents();
+    setTimeout(function () {
+      $(".faded").removeClass("faded");
+    },100);
   };
 
 
